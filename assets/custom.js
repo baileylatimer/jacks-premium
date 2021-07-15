60ae380044894641c8b45876cc615df2ca4e3d4b
Bailey Latimer <latimerdesignco@gmail.com>
2021-07-15 15:25:27 -0700

    product page / added product type label

---
+++ assets/custom.js
@@ -0,0 +1,6 @@
+​
+$(".yotpo-main-title").text(function () {
+  return $(this).text().replace("Other top rated products", "Even more to love"); 
+});​​​​​
+
+$( ".promoted-products-box" ).after( "<h1>Worth barking about</h1>" );
